93666edc5a8b5867b133a27fbf8f0c46f9235a84
goodwillparking <48186345+goodwillparking@users.noreply.github.com>
2021-03-07 14:50:10 -0500

    Add more event handler tests.

--- src/main/kotlin/com/github/goodwillparking/robokash/slack/event/Event.kt
+++ src/main/kotlin/com/github/goodwillparking/robokash/slack/event/Event.kt
@@ -16,7 +16,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 )
 sealed class Event
 
-data class EventWrapper<I : InnerEvent>(val event: I, val event_id: String) : Event()
+data class EventWrapper<I : InnerEvent>(val event: I, val event_id: String) : Event() {
+    fun innerCopy(block: I.() -> I) = copy(event = event.block())
+}
 
 data class UrlVerification(val challenge: String) : Event()
 
--- src/test/kotlin/com/github/goodwillparking/robokash/slack/SlackEventHandlerTest.kt
+++ src/test/kotlin/com/github/goodwillparking/robokash/slack/SlackEventHandlerTest.kt
@@ -12,54 +12,132 @@ import com.github.goodwillparking.robokash.slack.event.EventWrapper
 import io.kotest.core.spec.style.FreeSpec
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import java.time.Instant
 
 internal class SlackEventHandlerTest : FreeSpec({
 
     "bot should respond" - {
         "when it is mentioned" {
-            val handler = SlackEventHandler(
-                props = BotInstanceProperties(
-                    accessToken = "accessToken",
-                    signingSecret = "signingSecret",
-                    userId = UserId("botUser")
-                ),
-                slackInterface = mockk(),
-                random = mockk(),
-                responseProvider = { Responses("testResponse") },
-                responseProbability = 0.0
-            )
+            with(testHandler()) {
+                expectSuccessfulPost()
 
-            val event = EventWrapper(
-                ChatMessage(
-                    text = "Dude!",
-                    user = UserId("triggerUser"),
-                    channel = ChannelId("channel"),
-                    isMention = true
-                ),
-                "slackEventId"
-            )
+                handle(createRequest(chatMessageMention))
+                verifySuccessfulPost()
+            }
+        }
 
-            val body = serialize(event)
+        "when it rolls successfully" {
+            with(testHandler(probability = 0.1)) {
+                expectSuccessfulPost()
+                expectRoll(0.9)
 
-            val requestTime = Instant.now()
-            val signature = Auth.produceSignature(
-                key = handler.props.signingSecret,
-                body = body,
-                requestTime,
-                AUTH_VERSION
-            )
+                handle(createRequest(chatMessage))
+                verifySuccessfulPost()
+            }
+        }
 
-            val apiGatewayRequest = APIGatewayProxyRequestEvent()
-                .withBody(body)
-                .withHeaders(mapOf(
-                    TIMESTAMP_HEADER to requestTime.epochSecond.toString(),
-                    SIGNATURE_HEADER to signature
-                ))
+        "when probability is 1.0" {
+            with(testHandler(probability = 1.0)) {
+                expectSuccessfulPost()
+                expectRoll(0.0)
 
-            every { handler.slackInterface.postMessage(any(), event.event.channel) } returns Success("TADA!")
+                handle(createRequest(chatMessage))
+                verifySuccessfulPost()
+            }
+        }
+    }
 
-            handler.handleRequest(apiGatewayRequest, mockk())
+    "bot should not respond" - {
+        "when it rolls unsuccessfully" {
+            with(testHandler(probability = 0.1)) {
+                expectRoll(0.899999)
+                handle(createRequest(chatMessage))
+            }
+        }
+
+        "when it the probability is 0" {
+            with(testHandler(probability = 0.0)) {
+                expectRoll(1.0)
+                handle(createRequest(chatMessage))
+            }
+        }
+
+        "when the message is from the bot" {
+            with(testHandler(probability = 1.0)) {
+                val apiGatewayRequest = createRequest(chatMessage.innerCopy { copy(user = props.userId) })
+                expectRoll(1.0)
+                handle(apiGatewayRequest)
+            }
         }
     }
 })
+
+private val channelId = ChannelId("channel")
+
+private val chatMessage = EventWrapper(
+    ChatMessage(
+        text = "Dude!",
+        user = UserId("triggerUser"),
+        channel = channelId,
+        isMention = false
+    ),
+    "slackEventId"
+)
+
+private val chatMessageMention = EventWrapper(
+    ChatMessage(
+        text = "Dude!",
+        user = UserId("triggerUser"),
+        channel = channelId,
+        isMention = true
+    ),
+    "slackEventId"
+)
+
+private fun SlackEventHandler.handle(request: APIGatewayProxyRequestEvent) = handleRequest(request, mockk())
+
+private fun SlackEventHandler.expectRoll(result: Double) {
+    require(result >= 0)
+    require(result <= 1.0)
+    every { random.nextDouble(0.0, 1.0) } returns result
+}
+
+private fun SlackEventHandler.expectSuccessfulPost(channel: ChannelId = channelId) {
+    every { slackInterface.postMessage(any(), channel) } returns Success("It is done.")
+}
+
+private fun SlackEventHandler.verifySuccessfulPost(channel: ChannelId = channelId) {
+    verify(exactly = 1) { slackInterface.postMessage(any(), channel) }
+}
+
+private fun SlackEventHandler.createRequest(wrapper: EventWrapper<*>): APIGatewayProxyRequestEvent {
+    val body = serialize(wrapper)
+    val requestTime = Instant.now()
+
+    val signature = Auth.produceSignature(
+        key = props.signingSecret,
+        body = body,
+        requestTime,
+        AUTH_VERSION
+    )
+
+    return APIGatewayProxyRequestEvent()
+        .withBody(body)
+        .withHeaders(mapOf(
+            TIMESTAMP_HEADER to requestTime.epochSecond.toString(),
+            SIGNATURE_HEADER to signature
+        ))
+}
+
+private fun testHandler(probability: Double = 0.0) = SlackEventHandler(
+    props = BotInstanceProperties(
+        accessToken = "accessToken",
+        signingSecret = "signingSecret",
+        userId = UserId("botUser")
+    ),
+    slackInterface = mockk(),
+    random = mockk(),
+    responseProvider = { Responses("testResponse") },
+    responseProbability = probability
+)
