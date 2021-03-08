ce697e0c77c64268013d60d8f434d2d42d47f82e
goodwillparking <48186345+goodwillparking@users.noreply.github.com>
2021-03-07 19:05:32 -0500

    Read in filter file when processing raw messages.

--- src/test/kotlin/com/github/goodwillparking/robokash/tools/ResponseExtractor.kt
+++ src/test/kotlin/com/github/goodwillparking/robokash/tools/ResponseExtractor.kt
@@ -3,25 +3,35 @@ package com.github.goodwillparking.robokash.tools
 import com.fasterxml.jackson.core.type.TypeReference
 import com.github.goodwillparking.robokash.slack.Responses
 import com.github.goodwillparking.robokash.util.DefaultSerializer.objectMapper
-import com.github.goodwillparking.robokash.util.ResourceUtil
+import com.github.goodwillparking.robokash.util.ResourceUtil.loadTextResource
 import java.io.File
 import java.time.Instant
 
 private val leadingQuoteRegex = Regex("^&gt;\\s*")
 private val newlineQuoteRegex = Regex("\\n&gt;\\s*")
-private val timestampRegex = Regex("\\[.*\\d:\\d\\d.*]")
-// TODO: add blacklist to exclude specific messages, e.g. "Andrey:", "Q:", "Akash:"
+// TODO: Support "attachments"
+// TODO: Join with separate message file
 
 fun main(vararg args: String) {
     val outputDir = args[0]
 
-    val json = ResourceUtil.loadTextResource("/raw-messages.json")
+    val json = loadTextResource("/raw-messages.json")
     val messages = objectMapper.readValue(json, object : TypeReference<List<MessageWrapper>>() {})
         .map { it.message }
-    val responses = Responses(messages.map {
-        it.text.replace(leadingQuoteRegex, "")
-            .replace(newlineQuoteRegex, "\n")
-    }.filter { !it.contains(timestampRegex) })
+
+    val filter = objectMapper.readValue(
+        loadTextResource("/message-filter.json"),
+        MessageFilter::class.java
+    )
+
+    val responses = Responses(messages.asSequence()
+        .map {
+            it.text.replace(leadingQuoteRegex, "")
+                .replace(newlineQuoteRegex, "\n")
+        }
+        .filter { it !in filter.blockedMessages }
+        .filter { msg -> !filter.regexps.any { it in msg } }
+        .toList())
 
     println("${messages.size} source messages, ${responses.values.size} responses")
 
@@ -32,3 +42,5 @@ fun main(vararg args: String) {
 private data class MessageWrapper(val message: LoggedMessage, val timestamp: Instant)
 
 private data class LoggedMessage(val text: String)
+
+private data class MessageFilter(val blockedMessages: List<String>, val regexps: List<Regex>)
